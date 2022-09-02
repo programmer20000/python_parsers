dcd4aae39862ac4d33253617b91123101edfe08c
Mihai Apostol <84572292+programmer20000@users.noreply.github.com>
2022-09-02 13:20:43 +0300

    update version chrome driver

--- parser_playlist_video/unic.py
+++ parser_playlist_video/unic.py
@@ -1,7 +1,7 @@
-with open(file="music_2022/link_video.txt", mode="r+") as file:
+with open(file="sql/links_video.txt", mode="r+") as file:
     source = file.read()
     for i in set(source.split()):
         print(i)
-        with open(file="music_2022/link_video_unic.txt", mode="a", newline="") as f:
+        with open(file="sql/link_video_unic.txt", mode="a", newline="") as f:
             f.write(i + '\n')
 
