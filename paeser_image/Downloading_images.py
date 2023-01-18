1a80822f827a4350517f00bf694e90c980432c52
Mihai Apostol <mihaiapostol153@gmail.com>
2023-01-18 13:15:26 +0200

    Replace library for downloading image by link

--- paeser_image/Downloading_images.py
+++ paeser_image/Downloading_images.py
@@ -1,10 +1,21 @@
-import requests
+import os
+
+from icrawler.builtin import UrlListCrawler
 
 
-def Download_image():
-    with open(file="links_image_unic.txt", mode="r") as file:
-        source = file.read()
-        for links in source.split("\n"):
-            response =requests.get(url=links).content
+def Download_image(dir_name='image', file_path=''):
+    if os.path.exists(dir_name):
+        os.mkdir(dir_name)
+    else:
+        print('folder already exists')
 
-Download_image()
+        links_image = UrlListCrawler(downloader_threads=4, storage={'root_dir': dir_name})
+        links_image.crawl(url_list=file_path, file_idx_offset=0)
+
+
+def main():
+    Download_image(file_path='links_image_unic.txt')
+
+
+if __name__ == '__main__':
+    main()
